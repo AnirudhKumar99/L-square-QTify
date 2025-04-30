39b95cd109b351c5f35c539d342cc03c2940eb3c
AnirudhKumar99 <gvanirudh1999@gmail.com>
2025-04-30 08:28:04 +0530

    Adding NavBar and Hero Section

---
+++ qtify/src/components/FeedbackButton.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './FeedbackButton.css';
+
+function FeedbackButton() {
+  return (
+    <button className="feedback-button">Give Feedback</button>
+  );
+}
+
+export default FeedbackButton;
