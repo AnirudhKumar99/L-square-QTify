45fa3fb06bdb62f373f62875f664e78f8e436f1e
AnirudhKumar99 <gvanirudh1999@gmail.com>
2025-04-30 18:58:07 +0530

    fixing button issue

--- qtify/src/components/RightArrowButton.js
+++ qtify/src/components/RightArrowButton.js
@@ -1,11 +1,26 @@
+// import React from 'react';
+// import RightArrowIcon from '../assets/right_arrow.svg';
+// import './CarouselButton.css';
+
+// function RightArrowButton({ onClick }) {
+//   return (
+//     <button className="swiper-button-next" onClick={onClick}>
+//       <img src={RightArrowIcon} alt="Next" className="carousel-button" />
+//     </button>
+//   );
+// }
+
+
+// export default RightArrowButton;
+
 import React from 'react';
 import RightArrowIcon from '../assets/right_arrow.svg';
 import './CarouselButton.css';
 
 function RightArrowButton({ onClick }) {
   return (
-    <button className="swiper-button-next" onClick={onClick}>
-      <img src={RightArrowIcon} alt="Next" className="carousel-button" />
+    <button className="swiper-button-next carousel-nav-button next" onClick={onClick}>
+      <img src={RightArrowIcon} alt="Next" className="carousel-button" style={{ width: '30px', height: '30px' }} />
     </button>
   );
 }
